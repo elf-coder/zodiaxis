2f7c61b590125985000a1b844446b2de60c6215d
elf-coder <ibrahimpolat206@gmail.com>
2025-10-22 14:16:18 +0300

    Updated favicon to logo.png

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
         <title>Zodiaxis – Gerçek Gökyüzü Astrolojisi</title>
         <meta name="description" content="Zodiaxis: 13’lü Gerçek Gökyüzü Astrolojisi ve Aks Sistemi" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/png" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/logo.png" />
         <meta name="theme-color" content="#120b2f" />
       </Head>
       <Component {...pageProps} />
