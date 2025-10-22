dc710b2a5260a1b4656f356820b1e3e0430d9308
elf-coder <ibrahimpolat206@gmail.com>
2025-10-22 13:46:57 +0300

    globals.css yolu düzeltildi

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
         <title>Zodiaxis – Gerçek Gökyüzü Astrolojisi</title>
         <meta name="description" content="Zodiaxis: 13’lü Gerçek Gökyüzü Astrolojisi ve Aks Sistemi" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/png" href="/favicon-2025.png" />
+        <link rel="icon" type="image/png" href="/favicon.ico" />
         <meta name="theme-color" content="#120b2f" />
       </Head>
       <Component {...pageProps} />
