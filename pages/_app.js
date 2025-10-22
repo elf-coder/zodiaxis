1ff409d6a8be6338ac703ebc8d178a513e09882c
elf-coder <ibrahimpolat206@gmail.com>
2025-10-22 13:17:02 +0300

    favicon png olarak düzeltildi

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '../styles/globals.css';
+import '@/styles/globals.css'
+import Head from 'next/head'
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Zodiaxis – Gerçek Gökyüzü Astrolojisi</title>
+        <meta name="description" content="Zodiaxis: 13’lü Gerçek Gökyüzü Astrolojisi ve Aks Sistemi" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta name="theme-color" content="#120b2f" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
